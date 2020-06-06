30c31d20749fe19634c2821f8475ed2a4f02c4fd
Julie <julie.ngan68@gmail.com>
2020-06-06 18:07:09 +0200

    swipe fini

--- haircare.js
+++ haircare.js
@@ -1,6 +1,6 @@
 const _C  =  document.querySelector('.container'), 
       N  =  _C.children.length;
-      let i = 0, x0 = null, locked = false;
+      let i = 0, x0 = null, locked = false, w;
 
 
 
@@ -17,23 +17,31 @@ function lock(e) {
     _C.classList.toggle('smooth', !(locked = true)) 
 };
 
-
+function size() { w = window.innerWidth };
 
 function move(e) {
     console.log("moving");
   if(locked) {
       console.log("x0 = 0");
-    let dx = unify(e).clientX - x0, s = Math.sign(dx);
+    let dx = unify(e).clientX - x0, s = Math.sign(dx), 
+    f = +(s*dx/w).toFixed(2);
   
-    if((i > 0 || s < 0) && (i < N - 1 || s > 0))
-    console.log("i>0");
-    _C.style.setProperty('--i', i -= s);
+    if(((i > 0 || s < 0) && (i < N - 1 || s > 0))&& f >.2){
+        console.log("i>0");
+        _C.style.setProperty('--i', i -= s);
+        f = 1 - f
+    }
+    
     _C.style.setProperty('--tx', '0px');
+    _C.style.setProperty('--f', f);
     _C.classList.toggle('smooth', !(locked = false));
-	
     x0 = null
   }
-}
+};
+
+size();
+addEventListener('resize', size, false);
+
 _C.style.setProperty('--n', N)
 _C.addEventListener('mousedown', lock, false);
 _C.addEventListener('touchstart', lock, false);
