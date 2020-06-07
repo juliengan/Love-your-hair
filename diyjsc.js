e64b159c8143521811414cb7c216c4ffff907859
Aurélia <aurelia.ngau@gmail.com>
2020-06-07 18:00:39 +0200

    DIY swip ok

--- diyjsc.js
+++ diyjsc.js
@@ -1,21 +1,35 @@
-var slideIndex, slides, dots;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function initGallery(){
-	 slideIndex=0;
-	 slides=document.getElementsByClassName("imageHolder");
-	 slides[slideIndex].style.opacity=1;
-
-	 //add dots
-    dots=[];
-    var dotsContainer=document.getElementById("dotsContainer"),i;
-    for (i = 0; i < slides.length; i++) {
-        var dot=document.createElement("span");
-        dot.classList.add("dots");
-        dotsContainer.append(dot);
-        dot.setAttribute("onclick","moveSlide("+i+")");
-        dots.push(dot);
-    }
-    dots[slideIndex].classList.add("active");
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-initGallery();
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+  	slideIndex = 1
+  }
+
+  if (n < 1) {
+  	slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
