e08f825b868cdb5e2043e9db4a4f77d99ce2edbf
Julie <julie.ngan68@gmail.com>
2020-06-10 01:22:58 +0200

    ptit update

--- mobile.js
+++ mobile.js
@@ -1,8 +1,10 @@
 const navSlide =()=>{
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_burger'); 
+    const nav = document.querySelector('.nav_burger');
+    const navLinks = document.querySelectorAll('.nav_burger li');
     console.log(burger);
     console.log(nav);
+    console.log(navLinks);
     burger.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
         console.log('burger clicked');
